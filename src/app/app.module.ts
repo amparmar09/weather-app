5ca76791875cbfa1d48b33d9e6d08707fd7ab7e0
aman <amansinghal1702@gmail.com>
2024-08-12 18:14:13 +0530

    Working on google login using firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LoggingInterceptor } from './logging.interceptor';
 import { HeadersInterceptor } from './headers.interceptor';
 import { ErrorInterceptor } from './error.interceptor';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -19,7 +30,7 @@ import { ErrorInterceptor } from './error.interceptor';
     BrowserModule,
     AppRoutingModule,
     FormsModule,      
-    HttpClientModule,
+    HttpClientModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAnalytics(() => getAnalytics()), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideRemoteConfig(() => getRemoteConfig()), provideStorage(() => getStorage()),
   ],
   providers: [
   {
@@ -30,7 +41,8 @@ import { ErrorInterceptor } from './error.interceptor';
   },
   {
     provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true
-  }
+  },
+  ScreenTrackingService,UserTrackingService
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'weatherapp-1c768',
+    appId: '1:688917035787:web:9a9dd53409b59686cc400e',
+    databaseURL: 'https://weatherapp-1c768-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'weatherapp-1c768.appspot.com',
+    apiKey: 'AIzaSyCA5-_tF7wKpgSUtTdvm0wlBJvQOIrsCdw',
+    authDomain: 'weatherapp-1c768.firebaseapp.com',
+    messagingSenderId: '688917035787',
+    measurementId: 'G-QL8DLSBT4H',
+  },
     production: false,
     api_key: '3d4dfecc14a58ae944df4ff8d931acd1'
   };
