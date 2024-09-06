c7b8924f960155202aa9576ec2a24e03e8a2b179
amparmar09 <am.parmar097@gmail.com>
2024-09-06 11:46:34 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,3 @@
 export const environment = {
   firebase: {
-    projectId: 'weatherapp-1c768',
-    appId: '1:688917035787:web:9a9dd53409b59686cc400e',
-    databaseURL: 'https://weatherapp-1c768-default-rtdb.asia-southeast1.firebasedatabase.app',
-    storageBucket: 'weatherapp-1c768.appspot.com',
-    apiKey: '',
-    authDomain: 'weatherapp-1c768.firebaseapp.com',
-    messagingSenderId: '688917035787',
-    measurementId: 'G-QL8DLSBT4H',
-  },
-    production: false,
-    api_key: '3d4dfecc14a58ae944df4ff8d931acd1'
   };
